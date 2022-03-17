4f13d9332f2ef43524832bfbbc5b8614cd07fd12
AWTZA <1179681079Wtz@gmail.com>
2022-03-16 20:46:33 -0700

    code update

--- Code/Sensor_Controller/Sensor_Controller.ino
+++ Code/Sensor_Controller/Sensor_Controller.ino
@@ -1,9 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
-  
+
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  //Serial1.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
+  digitalWrite(LED_BUILTIN, LOW);    // switch off LED pin1
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // char receivedData
+  char receivedData;
 
+  while (Serial1.available())
+  {
+    int inByte = Serial1.read();
+    Serial1.print(inByte);
+    Serial1.print("abc");
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
