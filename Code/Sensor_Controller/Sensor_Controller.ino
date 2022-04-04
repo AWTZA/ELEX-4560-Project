bccb436222c42223b384008463be355dd39e5e0f
AWTZA <1179681079Wtz@gmail.com>
2022-04-04 09:01:18 -0700

    temp module finished

--- Code/Sensor_Controller/Sensor_Controller.ino
+++ Code/Sensor_Controller/Sensor_Controller.ino
@@ -9,43 +9,56 @@ void setup() {
   pinMode(16,OUTPUT);
   pinMode(18,OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);    // switch off LED pin1
-  digitalWrite(14, LOW);
-  digitalWrite(16, LOW);
-  digitalWrite(18, LOW);
+  digitalWrite(14, LOW); //status
+  digitalWrite(16, LOW); //alarm
+  digitalWrite(18, LOW); //yellow
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   // char receivedData
   char receivedData;
+  digitalWrite(14, HIGH); //status
 
   while (1)//Serial1.available()
   {
-    int inByte = Serial1.read();
+    //int inByte = Serial1.read();
 
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(10);
 
     int Message_In = Serial1.read();
+    if(Message_In == "A"){
+      digitalWrite(16, HIGH);
+      delay(200);
+    }
     //Serial1.print(inByte);
-    int Sound_Value = analogRead(A7);
-    int Water_Value = analogRead(A6);
-    //float voltage = sensorValue * (5.0 / 1023.0);
-    if(Sound_Value>220){
-      Serial1.println(Sound_Value);
-      delay(200);
-    }
-    if(Water_Value<700){
-      Serial1.println(Water_Value);
-      delay(200);
-    }
+    int Tem_value = analogRead(A7);
     
+    //float voltage = sensorValue * (5.0 / 1023.0);
+    Serial1.println(Tem_value);
+    if(Tem_value>35){
+      digitalWrite(16, HIGH);
+      delay(500);
+    }
     digitalWrite(LED_BUILTIN, LOW);
-    digitalWrite(16, LOW);
-    delay(200);
+    delay(500);
     digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(16, HIGH);
+    digitalWrite(16, LOW);
+    delay(500);
+ //   digitalWrite(LED_BUILTIN, LOW);
+    
     //delay(1000);
     
   }
 }
+
+/*
+ * 
+ *     int Water_Value = analogRead(A6);
+    if(Water_Value<700){
+      Serial1.println(Water_Value);
+      delay(200);
+    }
+
+*/
+ 
