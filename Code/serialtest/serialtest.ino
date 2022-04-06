dc6478116ea7d7372bacb9a5476d50382dfc318d
AWTZA <1179681079Wtz@gmail.com>
2022-04-06 04:48:21 -0700

    i

--- Code/serialtest/serialtest.ino
+++ Code/serialtest/serialtest.ino
@@ -1,9 +1,17 @@
+  #include <SoftwareSerial.h>
+
+
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(18, OUTPUT);
   //Serial1.begin(9600);
   //Serial1.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);      // set LED pin as output
+
+  
+
+
 
 }
 
@@ -11,10 +19,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   //digitalWrite(14, HIGH); //status
   digitalWrite(LED_BUILTIN, HIGH);
+ digitalWrite(18, HIGH);
   
-  Serial.println("123");
+  Serial1.println("123");
   delay(500);
   //digitalWrite(LED_BUILTIN, LOW);
+   digitalWrite(18, LOW);
     delay(500);
    // digitalWrite(LED_BUILTIN, HIGH);
 }
